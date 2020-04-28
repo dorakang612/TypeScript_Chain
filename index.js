f09fc2db00710c324ffff1c65cebdf749c2e97cc
An KangMin <hkkm612@gmail.com>
2020-04-28 16:11:48 +0900

    Add comments

--- index.js
+++ index.js
@@ -5,5 +5,6 @@ const sayHi = (name, age, gender) => {
     console.log(`Hello ${name}, you are ${age}, you are a ${gender}`);
 };
 sayHi(name, age);
+// 만약 sayHi()정의 시 gender에 ?를 붙이지 않았다면 TypeScript는 컴파일을 못하게 했을 겁니다.
 sayHi(name, age, gender);
 //# sourceMappingURL=index.js.map
--- index.ts
+++ index.ts
@@ -6,7 +6,10 @@ const sayHi = (name, age, gender?) => {
   console.log(`Hello ${name}, you are ${age}, you are a ${gender}`);
 };
 
+// 만약 sayHi()정의 시 gender에 ?를 붙이지 않았다면, javaScipt에서는 유연하게 undefined라고 하며 넘어 가겠지만, TypeScript는 컴파일을 못하게 했을 겁니다.
 sayHi(name, age);
-// 만약 sayHi()정의 시 gender에 ?를 붙이지 않았다면 TypeScript는 컴파일을 못하게 했을 겁니다.
+
 sayHi(name, age, gender);
+
+// TypeScript에게 모듈임을 알립니다.
 export {};
