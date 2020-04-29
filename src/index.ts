2c5f17323e8c7455c3fdfbca6b9e3b0a7dd0a422
An KangMin <hkkm612@gmail.com>
2020-04-29 20:03:42 +0900

    Class First Step

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,27 @@
-interface Human {
-  name: String;
-  age: Number;
-  gender: String;
+// 인터페이스는 자바스크립트로 컴파일되지 않습니다. 드물게 자바스크립트에 인터페이스 코드가 필요하다면, 인터페이스 대신 클래스를 사용합니다.
+class Human {
+  // public으로 변수들을 지정했기 때문에 클래스 외부에서도 접근 가능합니다.
+  // public이 아닌 private으로 지정한다면, 해당 변수는 해당 클래스 안에서만 사용 가능합니다.
+  public name: String;
+  public age: number;
+  public gender: string;
+
+  // 생성자. 클래스의 객체가 만들어질 때 호출되는 메서드.
+  constructor(name: string, age: number, gender: string) {
+    this.name = name;
+    this.age = age;
+    this.gender = gender;
+  }
 }
 
-const person = {
-  name: "kang",
-  age: 26,
-  gender: "male",
-};
+// kang은 Human클래스의 객체.
+const kang = new Human("kang", 26, "male");
 
-// 인자를 따로 따로 받을 땐 각각에 대해서 타입을 지정할 수 있었습니다. 함수에 객체를 넘기는 경우, 받아오는 인자는 한개 뿐이지만 담긴 정보는 여러개 이므로 인터페이스를 생성하여 타입들을 지정해 놓을 수 있습니다.
 const sayHi = (person: Human): String => {
-  return `Hello ${person.name}, you're ${person.age} and you're ${person.gender}`;
+  return `Hello ${person.name}, you're ${person.age}, and you're a ${person.gender}`;
 };
 
-console.log(sayHi(person));
+console.log(sayHi(kang));
 
 // TypeScript에게 모듈임을 알립니다.
 export {};
